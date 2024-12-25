28c9acb756503034a12c0302295d56027a986a45
billal mesloub <mesloubi2021@gmail.com>
2024-12-25 14:26:20 +0100

    Delete api directory
    Signed-off-by: billal mesloub <mesloubi2021@gmail.com>

--- api/controllers/main.ts
+++
@@ -1,8 +0,0 @@
-import main from "../services/main";
-
-const express = require('express');
-const router = express.Router();
-
-router.get('/', main)
-
-module.exports = router;
--- api/services/calculation/roundNum.ts
+++
@@ -1,5 +0,0 @@
-function roundNum(number: number, precision: number) {
-  return + (Math.round(+(number + 'e' + precision)) + 'e' + -precision);
-}
-
-export default roundNum;
