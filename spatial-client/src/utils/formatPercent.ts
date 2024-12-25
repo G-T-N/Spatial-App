143babbff3bdf23d5f23c633fc04fec3e1acccec
billal mesloub <mesloubi2021@gmail.com>
2024-12-25 14:34:17 +0100

    Delete spatial-client directory
    Signed-off-by: billal mesloub <mesloubi2021@gmail.com>

--- spatial-client/src/utils/formatPercent.ts
+++
@@ -1,6 +0,0 @@
-export default function valueFormatterPercentage(param: { value: number }) {
-    if (param.value === null || param.value === undefined) {
-        return '';
-    }
-    return `${param.value} %`;
-}
